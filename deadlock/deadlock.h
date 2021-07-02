c061d3d9c8a0480ee6220ef688725e1c58bc9b71
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-02 14:33:46 +0300

    Completed task deadlock

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,20 +1,31 @@
 #pragma once
 
+#include <mutex>
+#include <thread>
 
 class Deadlock {
  public:
-  Deadlock() {
-  }
+  Deadlock() {}
 
   void ThreadOne() {
     // Your
+    m1.lock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    m2.lock();
+    m2.unlock();
+    m1.unlock();
   }
 
   void ThreadTwo() {
     // Your
+    m2.lock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    m1.lock();
+    m1.unlock();
+    m2.unlock();
   }
 
  private:
+  std::mutex m1, m2;
   // Your
 };
-
