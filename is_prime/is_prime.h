d30f2560f11f1584dfca002caa9cfefa40c3a0da
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-02 13:16:36 +0300

    Completed task is_prime

--- is_prime/is_prime.h
+++ is_prime/is_prime.h
@@ -2,6 +2,18 @@
 
 #include <cstdint>
 
+// inline bool IsPrime(uint64_t x) {
+//   if (x == 1) {
+//     return false;
+//   }
+//   if (x == 2) {
+//     return true;
+//   }
+//   if (x % 2 == 0) {
+//     return false;
+//   }
+//   return true;
+// }
 
 inline bool IsPrime(uint64_t x) {
   if (x == 1) {
@@ -13,6 +25,10 @@ inline bool IsPrime(uint64_t x) {
   if (x % 2 == 0) {
     return false;
   }
+  for (uint64_t i = 2; i * i <= x; ++i) {
+    if (x % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
-
