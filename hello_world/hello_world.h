6c5a74f9dcf3577f95c6ede9f938c34c20dec3e2
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-02 14:12:10 +0300

    Completed task hello_world

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -1,23 +1,32 @@
 #pragma once
 
+#include <mutex>
 #include <ostream>
 #include <string_view>
-
+#include <thread>
+#include <vector>
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
-  {
-    // Your code
-  }
+  HelloWorld(size_t n_threads) : n_threads(n_threads) {}
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < n_threads; ++i) {
+      threads.emplace_back([&] {
+        m.lock();
+        os << kHelloPrefix << std::this_thread::get_id() << '\n';
+        m.unlock();
+      });
+    }
+    for (size_t i = 0; i < n_threads; ++i) {
+      threads[i].join();
+    }
   }
 
  private:
-  // Your code
+  size_t n_threads;
+  std::mutex m;
 };
-
