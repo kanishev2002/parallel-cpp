6a05598f961e068067c0d5669d9cab20568cd725
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-03 14:01:08 +0300

    Fixed hello_world

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -10,23 +10,22 @@ class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t n_threads) : n_threads(n_threads) {}
+  HelloWorld(size_t n_threads) : threads_count_(n_threads) {}
 
   void SayHello(std::ostream& os) {
     std::vector<std::thread> threads;
-    for (size_t i = 0; i < n_threads; ++i) {
+    for (size_t i = 0; i < threads_count_; ++i) {
       threads.emplace_back([&] {
-        m.lock();
+        std::unique_lock<std::mutex> lock(cout_mutex_);
         os << kHelloPrefix << std::this_thread::get_id() << '\n';
-        m.unlock();
       });
     }
-    for (size_t i = 0; i < n_threads; ++i) {
-      threads[i].join();
+    for (auto& thread : threads) {
+      thread.join();
     }
   }
 
  private:
-  size_t n_threads;
-  std::mutex m;
+  size_t threads_count_;
+  std::mutex cout_mutex_;
 };
