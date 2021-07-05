e539c7e8827226050af0313f8ae679e12426b645
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-05 15:18:43 +0300

    Fix thread_safe_queue

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -10,33 +10,25 @@
 template <typename T>
 class ThreadSafeQueue {
  public:
-  ThreadSafeQueue() : is_pushing_(false) {}
+  ThreadSafeQueue() {}
 
   void Push(const T& value) {
     std::unique_lock<std::mutex> lock(mutex_);
-    is_pushing_.store(true);
     queue_.push(value);
-    is_pushing_.store(false);
     cv_.notify_all();
   }
 
   T Pop() {
-    for (;;) {
-      std::unique_lock<std::mutex> lock(mutex_);
-      cv_.wait(lock, [&] { return !queue_.empty() || !is_pushing_; });
-      if (!is_pushing_ && !queue_.empty()) {
-        auto value = queue_.front();
-        queue_.pop();
-        return value;
-      }
-    }
+    std::unique_lock<std::mutex> lock(mutex_);
+    cv_.wait(lock, [&] { return !queue_.empty(); });
+    auto value = queue_.front();
+    queue_.pop();
+    return value;
   }
 
   std::optional<T> TryPop() {
     // Your code
-    while (is_pushing_) {
-      std::this_thread::yield();
-    }
+    std::unique_lock<std::mutex> lock(mutex_);
     if (queue_.empty()) {
       return std::nullopt;
     }
@@ -49,5 +41,4 @@ class ThreadSafeQueue {
   std::queue<T> queue_;
   std::mutex mutex_;
   std::condition_variable cv_;
-  std::atomic<bool> is_pushing_;
 };
