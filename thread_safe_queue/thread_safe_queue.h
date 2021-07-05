95f8f2b626fcb0530546779acbc4a6cdd40e5ae4
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-05 13:12:37 +0300

    Completed task threas_safe_queue

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -1,29 +1,42 @@
 #pragma once
 
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
 #include <optional>
 #include <queue>
-
+#include <thread>
 
 template <typename T>
 class ThreadSafeQueue {
  public:
-  ThreadSafeQueue() {
-  }
+  ThreadSafeQueue() : is_pushing_(false) {}
 
   void Push(const T& value) {
-    // Your code
+    std::unique_lock<std::mutex> lock(mutex_);
+    is_pushing_.store(true);
     queue_.push(value);
+    is_pushing_.store(false);
+    cv_.notify_all();
   }
 
   T Pop() {
-    // Your code
-    auto value = queue_.front();
-    queue_.pop();
-    return value;
+    for (;;) {
+      std::unique_lock<std::mutex> lock(mutex_);
+      cv_.wait(lock, [&] { return !queue_.empty() || !is_pushing_; });
+      if (!is_pushing_ && !queue_.empty()) {
+        auto value = queue_.front();
+        queue_.pop();
+        return value;
+      }
+    }
   }
 
   std::optional<T> TryPop() {
     // Your code
+    while (is_pushing_) {
+      std::this_thread::yield();
+    }
     if (queue_.empty()) {
       return std::nullopt;
     }
@@ -32,9 +45,9 @@ class ThreadSafeQueue {
     return value;
   }
 
-
  private:
-  // Your code
   std::queue<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable cv_;
+  std::atomic<bool> is_pushing_;
 };
-
