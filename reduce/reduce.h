ff3e2116096d4a6d8e7821bdb608a2b5b2bb9944
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-07 11:34:39 +0300

    Fixed task reduce

--- reduce/reduce.h
+++ reduce/reduce.h
@@ -10,9 +10,7 @@
 template <typename RandomAccessIterator, class T, class Func>
 T parallel_reduce(RandomAccessIterator begin, RandomAccessIterator end,
                   const T& initial_value, Func func, size_t n_threads) {
-  std::atomic<T> result{initial_value};
   const size_t size = static_cast<size_t>(end - begin);
-  // const size_t items_per_thread = size / n_threads;
   std::vector<std::thread> threads;
   std::vector<T> results(n_threads, initial_value);
   for (size_t i = 0; i < n_threads; ++i) {
@@ -21,7 +19,6 @@ T parallel_reduce(RandomAccessIterator begin, RandomAccessIterator end,
       auto from = begin + (i * size) / n_threads;
       auto to = begin + ((i + 1) * size) / n_threads;
       for (; from != to; ++from) {
-        // result.store(func(result.load(), *from));
         result = func(result, *from);
       }
       results[i] = result;
