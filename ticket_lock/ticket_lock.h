99333fa2266f87b03ceb558932c09afedc2ad131
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-10 21:04:59 +0300

    Completed task ticket_lock

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -1,20 +1,23 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
 
 class TicketLock {
  public:
-  TicketLock() {
-  }
+  TicketLock() {}
 
   void Lock() {
-    // Your code
+    size_t ticket = new_ticket_.load();
+    while (!new_ticket_.compare_exchange_weak(ticket, ticket + 1)) {
+    }
+    while (ticket != now_serving_.load()) {
+      std::this_thread::yield();
+    }
   }
 
-  void Unlock() {
-    // Your code
-  }
+  void Unlock() { ++now_serving_; }
 
  private:
-  // Your code
+  std::atomic<size_t> new_ticket_{0}, now_serving_{0};
 };
-
